b53841956035d8864c1501aadae4da751a99d4e7
DI GREGORIO Nicolas <nicolas.digregorio@gmail.com>
2020-08-04 19:36:54 +0200

    Initial release

---
+++ README.md
@@ -0,0 +1,72 @@
+# docker-nginx
+Installs NGINX into an CentOS Linux container
+
+![nginx](https://assets.wp.nginx.com/wp-content/themes/nginx-theme/assets/img//logo.png)
+
+## Description
+
+Nginx is software to provide a web server. It can act as a reverse proxy server for TCP, UDP, HTTP, HTTPS, SMTP, POP3, and IMAP protocols, as well as a load balancer and an HTTP cache.
+
+
+http://nginx.org/
+
+## Usage
+
+    docker create --name=nginx  \
+      -v /etc/localtime:/etc/localtime:ro \
+      -v <path to webroot>:/var/www/html/default \
+      -v <path to nginx logs>:/var/log/nginx  \
+      -e DOCKUID=<UID default:10002> \
+      -e DOCKGID=<GID default:10002> \
+      -e DOCKPHPFPMURL=<url-to-php-fpm default:php-fpm:9000> \
+      -e DOCKFCGIURL=<url-to-fastcgi default:fcgi:9001> \
+      -e DOCKSSL=<0|1 default:0> \
+      -e DOCKVHOST<default|php|symfony3-dev|symfony3-prd|symfony4|angular|grav|fcgi|custom default:default>
+      -p 80:80 \
+      -p 443:443 docker/docker-nginx
+
+## Environment Variables
+
+When you start the `nginx` image, you can adjust the configuration of the `nginx` instance by passing one or more environment variables on the `docker run` command line.
+
+### `DOCKUID`
+
+This variable is not mandatory and specifies the user id that will be set to run the application. It has default value `10002`.
+
+### `DOCKGID`
+
+This variable is not mandatory and specifies the group id that will be set to run the application. It has default value `10002`.
+
+### `DOCKPHPFPMURL`
+
+This variable is not mandatory and specifies the URL of a PHP FPM server. It has default value `php-fpm:9000`.
+
+### `DOCKFCGIURL`
+
+This variable is not mandatory and specifies the URL of a FastCGI server. It has default value `fcgi:9001`.
+
+### `DOCKSSL`
+
+This variable is not mandatory and specifies if the configured vhost will serve through https or http. It has default value `0`.
+
+### `DOCKVHOST`
+
+This variable is not mandatory and specifies to the entrypoint which vhost to configure. Valid values are `default`, `php`, `symfony3-dev`, `symfony3-prd`, `symfony4` and `angular`. It has default value `default`.
+* `default` will configure a vhost to serve static files.
+* `php` will configure a vhost that support php-fpm.
+* `symfony3-prd` will configure a vhost with support for symfony 3 production environment.
+* `symfony3-dev` will configure a vhost with support for symfony 3 development environment.
+* `symfony4` will configure a vhost with support for symfony 4 environment.
+* `angular` will configure a vhost with support for angular.
+* `grav` will configure a vhost with support for grav CMS.
+* `fcgi` will configure a vhost with support for FastCGI like spawn-fcgi.
+* `custom` will configure a vhost from a provided vhost config file `/docker-entrypoint.d/07-custom-http(s).conf`
+
+## Notes
+
+* The integrated nginx configuration redirects http to https and vice-versa depending the value of `DOCKSSL`.
+* The https vhost is using an autosigned certificate.
+* The image a builtin support for php-fpm, To correct the php-fpm destination, just add `-e DOCKPHPFPMURL=<url-to-php-fpm>` at container creation.
+* Avoiding to define `DOCKSSL` will result to let the entrypoint configure nginx with a regular vhost without SSL support.
+* Avoiding to define `DOCKVHOST` will result to let the entrypoint configure nginx with a regular vhost without support php-fpm.
+* The image supports adding bash droplets in /docker-entrypoint.d that will be launched at start up just before the launch of nginx.
---
+++ root/usr/local/bin/docker-entrypoint.sh
@@ -0,0 +1,129 @@
+#!/usr/bin/env bash
+
+. /etc/profile
+. /usr/local/bin/docker-entrypoint-functions.sh
+
+MYUSER="${APPUSER}"
+MYUID="${APPUID}"
+MYGID="${APPGID}"
+MYPHPFPMURL="php-fpm:9000"
+MYFCGIURL="fcgi:9001"
+MYSSL="0"
+MYVHOST="default"
+
+ConfigureUser
+
+if [ "${1}" == 'nginx' ]; then
+  MYSSLEXT=""
+  if [ -n  "${DOCKPHPFPMURL}" ]; then
+    MYPHPFPMURL="${DOCKPHPFPMURL}"
+  fi
+  if [ -n  "${DOCKFCGIURL}" ]; then
+    MYFCGIURL="${DOCKFCGIURL}"
+  fi
+  if [ -n  "${DOCKSSL}" ]; then
+    MYSSL="${DOCKSSL}"
+  fi
+  if [ -n  "${DOCKVHOST}" ]; then
+    MYVHOST="${DOCKVHOST}"
+  fi
+
+  if [ "${MYSSL}" == '1' ]; then
+    MYSSLEXT="https"
+  elif [ "${MYSSL}" == '0' ]; then
+    MYSSLEXT="http"
+  else
+    DockLog "ERROR: DOCKSSL value not supported, please check and try again."
+    exit 1
+  fi
+
+  DockLog "Disabling all vhosts"
+  rm -rf /etc/nginx/vhost-enabled/*
+
+
+  case "$MYVHOST" in
+    default)
+      DockLog "Enabling vhost: 00-default-${MYSSLEXT}"
+      cp /etc/nginx/vhost-disabled/00-default-${MYSSLEXT}.conf /etc/nginx/vhost-enabled/
+      ;;
+    php)
+      DockLog "Enabling vhost: 01-php-${MYSSLEXT}"
+      cp /etc/nginx/vhost-disabled/01-php-${MYSSLEXT}.conf /etc/nginx/vhost-enabled/
+      DockLog "Setting php-fpm URL to ${MYPHPFPMURL}"
+      sed -i \
+        -e "s|localhost:9000|${MYPHPFPMURL}|g" \
+      /etc/nginx/vhost-enabled/01-php-${MYSSLEXT}.conf
+      ;;
+    symfony3-prd)
+      DockLog "Enabling vhost: 02-symfony3-prd-${MYSSLEXT}"
+      cp /etc/nginx/vhost-disabled/02-symfony3-prd-${MYSSLEXT}.conf /etc/nginx/vhost-enabled/
+      DockLog "Setting php-fpm URL to ${MYPHPFPMURL}"
+      sed -i \
+        -e "s|localhost:9000|${MYPHPFPMURL}|g" \
+      /etc/nginx/vhost-enabled/02-symfony3-prd-${MYSSLEXT}.conf
+      ;;
+    symfony3-dev)
+      DockLog "Enabling vhost: 03-symfony3-dev-${MYSSLEXT}"
+      cp /etc/nginx/vhost-disabled/03-symfony3-dev-${MYSSLEXT}.conf /etc/nginx/vhost-enabled/
+      DockLog "Setting php-fpm URL to ${MYPHPFPMURL}"
+      sed -i \
+        -e "s|localhost:9000|${MYPHPFPMURL}|g" \
+      /etc/nginx/vhost-enabled/03-symfony3-dev-${MYSSLEXT}.conf
+      ;;
+    symfony4)
+      DockLog "Enabling vhost: 02-symfony4-${MYSSLEXT}"
+      cp /etc/nginx/vhost-disabled/02-symfony4-${MYSSLEXT}.conf /etc/nginx/vhost-enabled/
+      DockLog "Setting php-fpm URL to ${MYPHPFPMURL}"
+      sed -i \
+        -e "s|localhost:9000|${MYPHPFPMURL}|g" \
+      /etc/nginx/vhost-enabled/02-symfony4-${MYSSLEXT}.conf
+      if [ ! -d /var/www/html/default/public ]
+      then
+        mkdir -p /var/www/html/default/public
+      fi
+      ;;
+    angular)
+      DockLog "Enabling vhost: 04-angular-${MYSSLEXT}"
+      cp /etc/nginx/vhost-disabled/04-angular-${MYSSLEXT}.conf /etc/nginx/vhost-enabled/
+      ;;
+    grav)
+      DockLog "Enabling vhost: 05-grav-${MYSSLEXT}"
+      cp /etc/nginx/vhost-disabled/05-grav-${MYSSLEXT}.conf /etc/nginx/vhost-enabled/
+      sed -i \
+        -e "s|localhost:9000|${MYPHPFPMURL}|g" \
+      /etc/nginx/vhost-enabled/05-grav-${MYSSLEXT}.conf
+      ;;
+    fcgi)
+      DockLog "Enabling vhost: 06-fcgi-${MYSSLEXT}"
+      cp /etc/nginx/vhost-disabled/06-fcgi-${MYSSLEXT}.conf /etc/nginx/vhost-enabled/
+      sed -i \
+        -e "s|localhost:9001|${MYFCGIURL}|g" \
+      /etc/nginx/vhost-enabled/06-fcgi-${MYSSLEXT}.conf
+      ;;
+    custom)
+      DockLog "Enabling vhost: 07-custom-${MYSSLEXT}"
+      if [ -f /docker-entrypoint.d/07-custom-${MYSSLEXT}.conf ]; then
+        cp /docker-entrypoint.d/07-custom-${MYSSLEXT}.conf /etc/nginx/vhost-enabled/
+      else
+        DockLog "ERROR: /docker-entrypoint.d/07-custom-${MYSSLEXT}.conf not found"
+	      exit 1
+      fi
+      ;;
+    *)
+      DockLog "ERROR: DOCKVHOST value not supported, please check and try again."
+      exit 1
+      ;;
+  esac
+
+  RunDropletEntrypoint
+
+  DockLog "Setting permissions on /etc/nginx"
+  chown -R root:root /etc/nginx
+  DockLog "Setting permissions on /var/www/html/default"
+  chown -R "${MYUSER}" /var/www/html/default
+
+  DockLog "Starting app: ${1}"
+  exec nginx -c /etc/nginx/nginx.conf -g 'daemon off;'
+else
+  exec "$@"
+fi
