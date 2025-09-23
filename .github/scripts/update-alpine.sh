94ef2b9a827e7e4fe83e91b0006a444510e83c4e
DI GREGORIO Nicolas <ndigrego@ndg-consulting.tech>
2025-09-23 22:11:42 +0200

    update-alpine.sh workflow

--- .github/scripts/update-alpine.sh
+++ .github/scripts/update-alpine.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-ALPINELINUX_URL="https://api.github.com/repos/alpinelinux/aports/git/refs/tags"
+ALPINELINUX_URL="https://github.com/alpinelinux/aports.git"
 
-LAST_VERSION=$(curl -SsL https://api.github.com/repos/alpinelinux/aports/git/refs/tags | jq -c '.[] | select( .ref | contains("v3.") and (contains("rc") | not) and (contains("alpha") | not) and (contains("pre") | not))' | jq '.ref' -r| tail -1 | sed -e 's|refs/tags/v||')
-echo $LAST_VERSION
+LAST_VERSION=$(git ls-remote --tags ${ALPINELINUX_URL} | sed -e 's|.*refs/tags/v||' | grep -v '{}' | egrep "(^[0-9]+\.[0-9]+\.[0-9]+$)" | sort -V | tail -1)
 
-sed -i -e "s|FROM alpine:.*|FROM alpine:${LAST_VERSION}|" Dockerfile*
+if [ "${LAST_VERSION}" ];then
+  sed -i -e "s|FROM alpine:.*|FROM alpine:${LAST_VERSION}|" Dockerfile*
+fi
 
 if output=$(git status --porcelain) && [ -z "$output" ]; then
   # Working directory clean
